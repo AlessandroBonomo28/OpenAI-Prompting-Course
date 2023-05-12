99953a41f9edade533038a28b31aef6818326063
Alexbonny28 <al.bonomo@studenti.unina.it>
2023-05-13 00:02:40 +0200

    date difference improved

--- telegramAI/botAI.py
+++ telegramAI/botAI.py
@@ -7,7 +7,8 @@ _ = load_dotenv(find_dotenv())
 openai.api_key  = os.getenv('OPENAI_API_KEY')
 TOKEN_TELEGRAM = os.getenv('TOKEN_TELEGRAM')
 WHITELIST_ID_TELEGRAM = os.getenv("WHITELIST_ID_TELEGRAM").split(",") # IDs stored in .env as Comma Separated Values
-# Example: WHITELIST_ID_TELEGRAM = 123456,24566,344142
+# Example: WHITELIST_ID_TELEGRAM = "123456,24566,344142"
+USE_WHITELIST = True
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
@@ -29,24 +30,21 @@ Commands:\n
 2)/info ❔
 3)/time ⏳
 """
-
 def get_message_time_elapsed() -> str:
     now = datetime.datetime.now() 
-    seconds = abs(start_time - now).seconds
-    minutes = seconds/60
-    hours = minutes/60
-    days = abs(start_time - now).days
-    return f"""Long time passed since Yoda was born ⏳.\n\n{seconds} seconds\n{int(minutes)} minutes\n{int(hours)} hours\n{days} days\n"""
+    difference = abs(start_time - now)
+    hours, rest = divmod(difference.seconds, 3600)
+    minutes, seconds = divmod(rest, 60)
+    return f"""Long time passed since Yoda was born ⏳.\n\n{seconds} seconds\n{int(minutes)} minutes\n{int(hours)} hours\n{difference.days} days\n"""
 
 def send_info_commands(chat_id):
     bot.sendMessage(chat_id,infoMessage)
 
 def on_chat_message(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
-    if str(chat_id) not in WHITELIST_ID_TELEGRAM:
-        bot.sendMessage(chat_id, f"I can't share my knowledge with you ⛔.\nMaybe if you give this ID {chat_id} to right person I will...")
+    if USE_WHITELIST and str(chat_id) not in WHITELIST_ID_TELEGRAM:
+        bot.sendMessage(chat_id, f"I can't share my knowledge with you ⛔.\nMaybe if you give this ID {chat_id} to the right person I will...")
         return
-    #print(chat_id)
     if content_type == 'text':
         if msg["text"] == "/ping": 
             bot.sendMessage(chat_id, 'pong 🏓')
@@ -62,7 +60,7 @@ bot = telepot.Bot(TOKEN_TELEGRAM)
 bot.message_loop(on_chat_message)
     
 for id in WHITELIST_ID_TELEGRAM:
-    bot.sendMessage(id, f'May the force be with you..🍀')
+    bot.sendMessage(id, 'May the force be with you..🍀')
     send_info_commands(id)
 print ('Yoda listening ...')
 
